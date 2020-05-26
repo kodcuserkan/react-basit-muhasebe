27c51ffee8d6d5eee4eff9b063d982d20e1c2b55
Serkan Akman <serkan.akman@boranka.com>
2020-05-26 17:11:16 +0300

    Subfolder .env

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+require('dotenv').config();
+
+const username = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+const url = `mongodb+srv://${username}:${password}@muhasebecluster-nta6c.mongodb.net/test?retryWrites=true&w=majority`;
 
 const app = express();
+// Middlewarelar
+// app.use('/posts',()=> {
+//     console.log("Middleware")
+// })
 
+// Routelar
 app.get("/", (req,res) => {
     res.send("Home sayfamız");
+});
+app.get("/posts", (req,res) => {
+    res.send("Posts sayfamız");
 });
 
 app.listen(3333);
