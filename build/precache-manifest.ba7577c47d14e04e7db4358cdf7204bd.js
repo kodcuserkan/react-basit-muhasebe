c59d4e5d55c64c96f9e395ea847e795322c5c3ef
Serkan Akman <serkan.akman@boranka.com>
2020-05-26 13:55:40 +0300

    Build

---
+++ build/precache-manifest.ba7577c47d14e04e7db4358cdf7204bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7327e840860e2b20adce86784adef735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1a0e17abed111e7295d",
+    "url": "/static/css/main.be9551a2.chunk.css"
+  },
+  {
+    "revision": "9f75ecc8d796d31d8f3d",
+    "url": "/static/js/2.6f7444e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6f7444e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a0e17abed111e7295d",
+    "url": "/static/js/main.b2f4aed1.chunk.js"
+  },
+  {
+    "revision": "d4d1decf33b4c374b1d2",
+    "url": "/static/js/runtime-main.7f18eedf.js"
+  }
+]);
